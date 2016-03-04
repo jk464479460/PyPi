e204281f7f47102255d51b3ea42a0bd5879d6722
jk464479460 <464479460@qq.com>
2016-03-04 16:58:08 +0800

    建立学习字典
    一个输入字典；一个查询。刚刚学习3天。

--- akka.py
+++ akka.py
@@ -3,9 +3,14 @@ if __name__=='__main__':
         f=open('akkaDict.txt','a')
         while(True):
                 a=input()
+                b=input()
                 if(a=='over'):
                         break
+                if(b=='exit'):
+                        print('cacel text')
+                        continue;
                 print(a,file=f)
+                print('go on',end='')
 
         f.close()
         
---
+++ learakka.py
@@ -0,0 +1,34 @@
+def FindIt():
+     path='''akkaDict.txt''';
+     f=open(path, 'r');
+     allLine=f.readlines()
+     f.close()
+     print(len(allLine))
+     while True:
+          inputStr=input();
+          #print('input %s' %inputStr)
+          if(isinstance(inputStr, str)==False):
+               print('must be str')
+               continue
+          if(inputStr=='n'):
+               return
+          if(inputStr=="all"):
+               for line in allLine:
+                    print(line,end='')
+               print('all show',end='')
+               
+          for line in allLine:
+               if(line.find(inputStr,0,len(inputStr))!=-1):
+                    print(line)
+                    break
+                  
+     
+
+if '__main__'==__name__:
+   FindIt()
+
+     
+     
+        
+    
+    
